c31018a803b76d96b302a5666c8055502055879b
Ignatiy Voronov <ignatiy.voronov@gmail.com>
2026-04-09 17:02:32 +0200

    Nvim: Migrate to new treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,12 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
+  "romus204/tree-sitter-manager.nvim",
+  dependencies = {}, -- tree-sitter CLI must be installed system-wide
   config = function()
-    require("nvim-treesitter.configs").setup {
-      ensure_installed = {"bash"," go", "gomod", "gowork", "gosum", "lua", "javascript", "typescript"},
-      sync_install = false,
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true,
-      },
-    }
-  end,
-  dependencies = {
-    {'nvim-treesitter/nvim-treesitter-context'},
-  }
+    require("tree-sitter-manager").setup({
+      -- Optional: custom paths
+      -- parser_dir = vim.fn.stdpath("data") .. "/site/parser",
+      -- query_dir = vim.fn.stdpath("data") .. "/site/queries",
+    })
+  end
 }
 
