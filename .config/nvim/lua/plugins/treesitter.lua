5a6f3d985fd806b169b4cb3f4c00c18579f889e1
Ignatiy Voronov <ignatiy.voronov@gmail.com>
2026-04-19 15:40:01 +0200

    Nvim: Add treesitter manager and context

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,20 @@
-return {
-  "romus204/tree-sitter-manager.nvim",
-  dependencies = {}, -- tree-sitter CLI must be installed system-wide
-  config = function()
-    require("tree-sitter-manager").setup({
-      -- Optional: custom paths
-      -- parser_dir = vim.fn.stdpath("data") .. "/site/parser",
-      -- query_dir = vim.fn.stdpath("data") .. "/site/queries",
-    })
-  end
-}
+vim.pack.add({
+  {
+    -- tree-sitter CLI must be installed system-wide
+    src = "https://github.com/romus204/tree-sitter-manager.nvim",
+  },
+  {
+    src = "https://github.com/nvim-treesitter/nvim-treesitter-context",
+  },
+})
 
+require("tree-sitter-manager").setup({
+  -- Optional: custom paths
+  -- parser_dir = vim.fn.stdpath("data") .. "/site/parser",
+  -- query_dir = vim.fn.stdpath("data") .. "/site/queries",
+  ensure_installed = {"go","typescript"},
+})
+
+require("treesitter-context").setup({
+  max_lines = 3,
+})
