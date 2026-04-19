0c0f7234cbc049503894c5ababdf6b0b0fc14c4e
Ignatiy Voronov <ignatiy.voronov@gmail.com>
2026-04-19 15:49:24 +0200

    Fish: Move claude to aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,8 +10,6 @@ zoxide init fish | source
 fish_add_path "$HOME/.local/bin"
 
 
-alias claude="/Users/user/.claude/local/claude"
-
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
