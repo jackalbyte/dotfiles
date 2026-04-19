74bd9db11eaae9572d7af51ce213b70c7cb74f24
Ignatiy Voronov <ignatiy.voronov@gmail.com>
2026-04-19 15:39:32 +0200

    Nvim: Delete fugitive

--- .config/nvim/lua/plugins/fugitive.lua
+++
@@ -1,5 +0,0 @@
-vim.pack.add({'https://github.com/tpope/vim-fugitive'})
-
-vim.keymap.set("n", "<leader>vs", ":tab Git<CR>", { desc = "Git Tab" })
-vim.keymap.set("n", "<leader>va", ":Git blame<CR>", { desc = "Git Blame" })
-
