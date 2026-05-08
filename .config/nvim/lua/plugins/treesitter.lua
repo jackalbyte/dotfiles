3903e9ccc668009104c2e76ee4ebd896a2ece29a
Ignatiy Voronov <ignatiy.voronov@gmail.com>
2026-05-08 17:15:14 +0200

    Nvim: Treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,13 @@
 vim.pack.add({
-  {
-    -- tree-sitter CLI must be installed system-wide
-    src = "https://github.com/romus204/tree-sitter-manager.nvim",
-  },
-  {
-    src = "https://github.com/nvim-treesitter/nvim-treesitter-context",
-  },
+  { src = "https://github.com/romus204/tree-sitter-manager.nvim" }, -- tree-sitter CLI must be installed system-wide 
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter-context" },
 })
 
 require("tree-sitter-manager").setup({
   -- Optional: custom paths
   -- parser_dir = vim.fn.stdpath("data") .. "/site/parser",
   -- query_dir = vim.fn.stdpath("data") .. "/site/queries",
-  ensure_installed = {"go","typescript"},
+  ensure_installed = {"go","typescript", "php", "php_only"},
 })
 
 require("treesitter-context").setup({
