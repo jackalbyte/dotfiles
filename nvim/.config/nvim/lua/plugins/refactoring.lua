5d35104e2977758140584c498b9c6dedac1e56b5
jackalbyte <ignatiy.voronov@gmail.com>
2026-05-26 17:27:22 +0200

    Nvim: Fix refactoring deps

--- nvim/.config/nvim/lua/plugins/refactoring.lua
+++ nvim/.config/nvim/lua/plugins/refactoring.lua
@@ -1,6 +1,7 @@
 vim.pack.add({
   { src = "https://github.com/nvim-lua/plenary.nvim" },
   { src = "https://github.com/ThePrimeagen/refactoring.nvim" },
+  { src = "https://github.com/lewis6991/async.nvim" },
 })
 
 require("refactoring").setup()
